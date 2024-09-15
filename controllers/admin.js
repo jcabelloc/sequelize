c028c37eb6d7b0c696dd5e547ff105389b364ba3
Juan Cabello <jcabelloc@itana.pe>
2024-09-14 21:45:26 -0500

    Obtener, Actualizar y Eliminar Productos

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,7 +13,6 @@ exports.postCrearProducto = (req, res, next) => {
   const urlImagen = req.body.urlImagen;
   const precio = req.body.precio;
   const descripcion = req.body.descripcion;
-  const producto = new Producto(null, nombre, urlImagen, descripcion, precio);
   Producto.create({
     nombre: nombre,
     precio: precio,
@@ -37,7 +36,8 @@ exports.getEditarProducto = (req, res, next) => {
     return res.redirect('/');
   }
   const idProducto = req.params.idProducto;
-  Producto.findById(idProducto, producto => {
+  Producto.findByPk(idProducto)
+  .then(producto => {
     if (!producto) {
       return res.redirect('/');
     }
@@ -47,7 +47,8 @@ exports.getEditarProducto = (req, res, next) => {
       modoEdicion: true,
       producto: producto
     });
-  });
+  })
+  .catch(err => console.log(err));
 };
 
 exports.postEditarProducto = (req, res, next) => {
@@ -56,29 +57,42 @@ exports.postEditarProducto = (req, res, next) => {
   const precio = req.body.precio;
   const urlImagen = req.body.urlImagen;
   const descripcion = req.body.descripcion;
-  const productoActualizado = new Producto(
-    idProducto,
-    nombre,
-    urlImagen,
-    descripcion,
-    precio
-  );
-  productoActualizado.save();
-  res.redirect('/admin/productos');
+  Producto.findByPk(idProducto)
+  .then(producto => {
+    producto.nombre = nombre;
+    producto.precio = precio;
+    producto.descripcion = descripcion;
+    producto.urlImagen = urlImagen;
+    return producto.save();
+  })
+  .then(result => {
+    console.log('PRODUCTO ACTUALIZADO!');
+    res.redirect('/admin/productos');
+  })
+  .catch(err => console.log(err));
 };
 
 exports.getProductos = (req, res, next) => {
-    Producto.fetchAll(productos => {
-      res.render('admin/productos', {
-        prods: productos,
-        titulo: 'Admin Productos',
-        path: '/admin/productos'
-      });
+  Producto.findAll()
+  .then(productos => {
+    res.render('admin/productos', {
+      prods: productos,
+      titulo: 'Admin Productos',
+      path: '/admin/productos'
     });
+  })
+  .catch(err => console.log(err));
   };
 
 exports.postEliminarProducto = (req, res, next) => {
   const idProducto = req.body.idProducto;
-  Producto.deleteById(idProducto);
-  res.redirect('/admin/productos');
+  Producto.findByPk(idProducto)
+  .then(producto => {
+    return producto.destroy();
+  })
+  .then(result => {
+    console.log('PRODUCTO ELIMINADO');
+    res.redirect('/admin/productos');
+  })
+  .catch(err => console.log(err));
 };
